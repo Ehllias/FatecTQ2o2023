4a4ea01bbd8e2786688f5604a28f9a5ff797dd90
Ehllias <ehllias.ramalho@fatec.sp.gov.br>
2023-11-22 03:04:20 -0300

    Criação das variáveis js.

---
+++ script.js
@@ -0,0 +1,3 @@
+var nome = document.querySelector('.text').classList.add('name');
+var email = document.querySelector('.email').classList.add('email');
+var message = document.querySelector('.message').classList.add('message');
